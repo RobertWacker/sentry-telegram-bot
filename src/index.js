e177bec9195eb8ae0e9e7d7a64a238fd5911c9c7
Wacker <RobertWacker>
2020-01-14 18:54:57 +0700

    Added example webhook and Procfile for deployment on heroku

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const Telegraf = require('telegraf')
+const fastifyApp = require('fastify')()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.on('text', ({ reply }) => reply('Hello'))
+fastifyApp.use(bot.webhookCallback('/secret-path'))
+// Set telegram webhook
+// npm install -g localtunnel && lt --port 3000
+bot.telegram.setWebhook('https://------.localtunnel.me/secret-path')
+
+fastifyApp.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})

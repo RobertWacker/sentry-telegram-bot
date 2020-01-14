b13eb5fbc325e3a008c0f4ad8eb98f3c8681fa75
Wacker <RobertWacker>
2020-01-14 19:07:12 +0700

    Add webhook

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 BOT_TOKEN - Your bot token obtained from FatherBot
 DSN - 
+
+
+Settings > Project > Developer Settings > Edit Integration
+Получаем Токен и кредшиналс в sentery
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ bot.on('text', ({ reply }) => reply('Hello'))
 fastifyApp.use(bot.webhookCallback('/secret-path'))
 // Set telegram webhook
 // npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook('https://------.localtunnel.me/secret-path')
+bot.telegram.setWebhook('kinobu-sentry.herokuapp.com')
 
 fastifyApp.listen(3000, () => {
   console.log('Example app listening on port 3000!')

f9a1979aa6a28ce4ec54ff73bd72145f785bb9dd
Wacker <RobertWacker>
2020-01-14 23:28:09 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const Telegraf = require('telegraf')
 const fastifyApp = require('fastify')()
 
-const bot = new Telegraf(process.env.BOT_TOKEN || '1023212388:AAHOrrjkD1yD_iK457-gx065ssmKJzYlsy0')
+const TELEGRAM_TOKEN = process.env.BOT_TOKEN || '1023212388:AAHOrrjkD1yD_iK457-gx065ssmKJzYlsy0'
+
+const bot = new Telegraf(TELEGRAM_TOKEN)
 
 bot.on('text', ({ reply }) => reply('Hello'))
 fastifyApp.use(bot.webhookCallback('/secret-path'))
 // Set telegram webhook
 // npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook('https://kinobu-sentry.herokuapp.com/secret-path')
+bot.telegram.setWebhook(`https://kinobu-sentry.herokuapp.com/secret-path/${TELEGRAM_TOKEN}`)
 
 fastifyApp.route({
-    method: 'GET',
+    method: 'POST',
     url: '/secret-path',
     schema: {
       querystring: {
